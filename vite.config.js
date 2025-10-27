b23c6b5eb23b78ff0dcb47dbbe9f84553e217e7f
maryemderbali <derbalimaryem@gmail.com>
2025-10-27 22:47:51 +0100

    ready for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/Portfolio/" : "/",
+  base: "/Portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
