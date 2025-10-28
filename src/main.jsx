90ecc2b07edc78512c4f25fde69acba1ae777c23
maryemderbali <derbalimaryem@gmail.com>
2025-10-28 14:18:32 +0100

    ready for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom"; // Use HashRouter for GH Pages
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <HashRouter>
